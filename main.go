86ffca77730ceda675046e2f85a26b91e42bf1ad
sneakstarberry <gihong2012@likelion.org>
2020-04-11 04:05:04 +0900

    test

--- main.go
+++ main.go
@@ -2,32 +2,21 @@ package main
 
 import "fmt"
 
-func nojam11050() {
-	var a int
-	var b int
-
-	fmt.Scanln(&a)
-	fmt.Scanln(&b)
-	var FacA = 1
-	var FacB = 1
-	var FacC = 1
-	for i := 1; i <= a; i++ {
-		FacA *= i
-	}
-	for j := 1; j <= b; j++ {
-		FacB *= j
-	}
-	for k := 1; k <= (a - b); k++ {
-		FacC *= k
-	}
-	var resultA int
-	fmt.Println(FacA)
-	fmt.Println(FacB)
-	fmt.Println(FacC)
-	resultA = FacA / (FacB * FacC)
-	fmt.Println(resultA)
-}
+var (
+	num1   int
+	float1 float32
+	exam1  string
+)
 
 func main() {
-	nojam11050()
+	num1 = 10
+	float1 = 5.23
+	exam1 = "Hello World"
+
+	num2 = 10
+	float2
+
+	fmt.Println(num)
+	fmt.Println(float)
+	fmt.Println(exam)
 }
