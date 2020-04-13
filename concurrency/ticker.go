7f118b82f2baa067543735f7c1b0bc4a9d9f981e
sneakstarberry <gihong2012@likelion.org>
2020-04-14 04:14:32 +0900

    select ticker

---
+++ concurrency/ticker.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func push(c chan int) {
+	i := 0
+	for {
+		time.Sleep(1 * time.Second)
+		c <- i
+		i++
+	}
+}
+
+func main() {
+	c := make(chan int)
+
+	go push(c)
+
+	timer := time.After(10 * time.Second)
+	tickTimerChan := time.Tick(2 * time.Second)
+
+	for {
+		select {
+		case v := <-c:
+			fmt.Println(v)
+		case <-timer:
+			fmt.Println("timeout")
+			return
+		case <-tickTimerChan:
+			fmt.Println("Tick")
+		}
+	}
+}
